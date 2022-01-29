b04b666a94560f46a8ab96e7fbdf14b0825162b5
Jetson Nano <fritingo.ai10@nycu.edu.tw>
2022-01-29 20:27:55 +0800

    upload demo file

--- pill/yolov4/load_path.py
+++ pill/yolov4/load_path.py
@@ -1,5 +1,5 @@
 import glob
 
-with open('train.txt','w') as f:
-    for item in glob.glob('/home/fritingo/Documents/pill_data/all/*.png'):
+with open('jetson_train.txt','w') as f:
+    for item in glob.glob('/home/medical/pill_data/all/*.png'):
         f.write('%s\n' % item)
