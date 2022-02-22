b2a0f427a8fa09540055100fd341ce3c67e74596
Kai Chun <34733986+kigison@users.noreply.github.com>
2022-02-22 22:46:03 +0800

    upload

---
+++ Arduino_code/light/light.ino
@@ -0,0 +1,15 @@
+void setup() {
+  pinMode(11,OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Serial bound rate is '9600'");
+  Serial.println("Please input the value of Luminous(0~255).");
+}
+int var_light = 10;
+void loop() {
+  Serial.print("Luminous value: ");
+  Serial.println(var_light);
+  analogWrite(11, var_light);
+  while(!Serial.available()){}
+  String var_read = Serial.readString();
+  var_light = var_read.toInt();
+}
