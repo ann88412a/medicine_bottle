6209b1ba9812d9d55431578a33203f0f19048135
kigison <ken888865@gmail.com>
2022-07-30 22:42:09 +0800

    rewrite the env_light's arduino code

--- Arduino_code/env_light/env_light.ino
+++ Arduino_code/env_light/env_light.ino
@@ -1,18 +1,31 @@
-const int analogOutPin = 5;
-// Analog output pin that the LED is attached to
-const int reset = 4;
-    
-int outputValue = 0;        // value output to the PWM (analog out)
+#define light_pin 5 //define the light hardware pin is D5(PWM)
+#define serial_bound_rate 115200 //defint the bound rate is 115200
+uint8_t var_light = 10; //init val is 10
+String serial_read = ""; //init
+bool DEBUG_MODE = false; //serial print info (if true)
 
 void setup() {
-  pinMode(reset,INPUT);
-  analogWrite(analogOutPin, 3);
+  pinMode(light_pin, OUTPUT); //set light pin is output
+  Serial.begin(serial_bound_rate); //set bound rate
+  Serial.setTimeout(10); //set serial timeout (readString wait time)
+  analogWrite(light_pin, var_light); //init the light
+  if (DEBUG_MODE) {
+    Serial.println("Serial bound rate is '" + String(serial_bound_rate) + "'");
+    Serial.println("Please input the value of Luminous(0~255).");
+  }
 }
 
 void loop() {
-  if (digitalRead(reset)){
-    analogWrite(analogOutPin,0);
-  }else{
-    analogWrite(analogOutPin,3);
+  if (serial_read.length() > 2) {
+    var_light = serial_read.toInt(); //str => int
+    analogWrite(light_pin, var_light); //set light val
+
+    if (DEBUG_MODE) {
+      Serial.println("Luminous value: " + String(var_light)); // print the light val
+    }
   }
+
+  while (!Serial.available()) {} //wait for input
+  serial_read = Serial.readString(); //read input
+
 }
