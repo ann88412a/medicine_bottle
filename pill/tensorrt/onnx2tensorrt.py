9dd69618c7849988e115b45510dfd0821a6927d7
Jetson Nano <fritingo.ai10@nycu.edu.tw>
2023-03-19 17:19:11 +0800

    device testing

--- pill/tensorrt/onnx2tensorrt.py
+++ pill/tensorrt/onnx2tensorrt.py
@@ -1,17 +1,19 @@
 import tensorrt as trt
 
 
-onnx_file = 'yolov7-tiny.onnx'
-trt_file = 'yolov7-tiny.trt'
+onnx_file = 'pill/tensorrt/yolov7-tiny.onnx'
+trt_file = 'pill/tensorrt/yolov7-tiny.trt'
 batch_size = 1
 
 TRT_LOGGER = trt.Logger(trt.Logger.WARNING)
 EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
 """Takes an ONNX file and creates a TensorRT engine to run inference with"""
 with trt.Builder(TRT_LOGGER) as builder, builder.create_network(EXPLICIT_BATCH) as network, trt.OnnxParser(network, TRT_LOGGER) as parser:
-    builder.max_workspace_size = 1<<28 # 256MiB
+    # builder.max_workspace_size = 1<<28 # 256MiB
+    config = builder.create_builder_config()
+    config.max_workspace_size = 1 << 28
     builder.max_batch_size = batch_size
-    builder.fp16_mode = True # fp32_mode -> False
+    config.fp16_mode = True # fp32_mode -> False
     # Parse model file
     with open(onnx_file, 'rb') as model:
         print('Beginning ONNX file parsing')
