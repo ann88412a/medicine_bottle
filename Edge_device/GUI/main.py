f8f1b3dd37c3549006553b030e84c79add2d1ad2
kigison <ken888865@gmail.com>
2023-03-30 23:52:43 +0800

    修正部分小問題

--- Edge_device/GUI/main.py
+++ Edge_device/GUI/main.py
@@ -15,39 +15,48 @@ from . import DAN
 class medical_GUI:
     def __init__(self, cfg_file_path="./config_files/GUI_default.cfg"):
         self.DEBUG = True
-        ## Load config file
-        with open(cfg_file_path, 'r') as __f:
-            self.__cfg = json.load(__f)
-            __f.close()
-        ## instantiated the other func class
-        self.syringe_scale = syringe_scale(self.__cfg)
-        self.light = light_control(self.__cfg["arduino_serial_com_port"], self.__cfg["arduino_serial_baud_rates"])
-        self.webcam_stream = medical_webcam_stream()
-        ## start the IoTtalk DAI loop
-        dm_loop = Thread(target=self.dummy_device_loop, name="medical_iottalk")
-        dm_loop.setDaemon(True)
-        dm_loop.start()
-        ## create the window
-        self.window = tk.Tk()
-        self.window.title('Medical Talk')
-        if self.DEBUG:
-            self.window.geometry("1024x600")
-        else:
-            self.window.attributes("-fullscreen", True)
-            self.window.attributes("-topmost", True)
-        self.window.update()
-        self.window.bind('<Escape>', lambda _: self.quit())  # press "Esc" key to close
-        self.window.bind('<ButtonPress-1>', self.sys_options_long_press_event)  # long press to quit (3s)
-        self.window.bind('<ButtonRelease-1>', self.sys_options_long_press_event)
-        self.__screen_width = self.window.winfo_width()
-        self.__screen_height = self.window.winfo_height()
-        self.__font_ratio = self.__screen_width / 1024
-        if self.DEBUG:
-            print("Screen size(w*h): {} * {}".format(self.__screen_width, self.__screen_height))
+        try:
+            ## Load config file
+            with open(cfg_file_path, 'r') as __f:
+                self.__cfg = json.load(__f)
+                __f.close()
+            ## instantiated the other func class
+            self.syringe_scale = syringe_scale(self.__cfg)
+            self.light = light_control(self.__cfg["arduino_serial_com_port"], self.__cfg["arduino_serial_baud_rates"])
+            self.webcam_stream = medical_webcam_stream()
+            ## start the IoTtalk DAI loop
+            dm_loop = Thread(target=self.dummy_device_loop, name="medical_iottalk")
+            dm_loop.setDaemon(True)
+            dm_loop.start()
+            ## create the window
+            self.window = tk.Tk()
+            self.window.title('Medical Talk')
+            if self.DEBUG:
+                self.window.geometry("1024x600")
+            else:
+                self.window.attributes("-fullscreen", True)
+                self.window.attributes("-topmost", True)
+            self.window.update()
+            self.window.bind('<Escape>', lambda _: self.quit())  # press "Esc" key to close
+            self.window.bind('<ButtonPress-1>', self.sys_options_long_press_event)  # long press to quit (3s)
+            self.window.bind('<ButtonRelease-1>', self.sys_options_long_press_event)
+            self.__screen_width = self.window.winfo_width()
+            self.__screen_height = self.window.winfo_height()
+            self.__font_ratio = self.__screen_width / 1024
+            if self.DEBUG:
+                print("Screen size(w*h): {} * {}".format(self.__screen_width, self.__screen_height))
+        except BaseException as e:
+            print("Failure to Start.")
+            print("Err Msg:", e)
+            exit()
 
     def run(self):
-        ## run check all state before start
+        ## run check all states are ready before start
         self.start_up_check()
+        ## first page
+        self.wait_page()  # first step
+        # self.scan_scale()
+        # self.pill_detector()
         ## run GUI
         self.window.mainloop()
         ## release cam after done
@@ -99,20 +108,78 @@ class medical_GUI:
             assert self.check_network(URL=self.__cfg["ServerURL"], show_GUI=False), "Failed to connect to the IoTtalk server!"
             assert self.light.check_light_state(), "Failed with the light control serial error!"
             assert self.webcam_stream.check_cam_ready(), "Failed with the webcam streaming error!"
-            self.wait_page()  # first step
-            # self.scan_scale()
         except AssertionError as error:
             self.window.destroy()
             print("#############################")
             print(error)
             print("#############################")
-
+            exit()
         except BaseException as e:
             self.window.destroy()
             print("#############################")
             print("Failed with an unknown error!")
             print("Error msg:", e)
             print("#############################")
+            exit()
+
+    def check_network(self, URL="https://www.google.com.tw/", show_GUI=True):
+        self.clean()
+        # retry_time_count = time.time()
+        try:
+            urllib.request.urlopen(URL, timeout=1)
+            if show_GUI:
+                self.wait_page()
+            else:
+                return True
+        except urllib.request.URLError as err:
+            if show_GUI:
+                flg = tk.Label(self.window, text="")
+                tk.Label(self.window, text=self.text_translate("系統已離線..."), font=('', int(80 * self.__font_ratio), 'bold')).place(relx=0.0, rely=0.0, relwidth=1.0, relheight=1.0)
+                tk.Button(self.window, text=self.text_translate("retry"), font=('', int(40 * self.__font_ratio), 'bold'),
+                          command=flg.pack).place(relx=0.8, rely=0.8, relwidth=0.2, relheight=0.2)
+                self.window.wait_visibility(flg)
+                self.check_network(URL)
+            else:
+                return False
+
+    def dummy_device_loop(self):
+        ServerURL = self.__cfg["ServerURL"]
+        Reg_addr = self.__cfg["Reg_addr"]  # if None, Reg_addr = MAC address
+        DAN.profile['dm_name'] = "medical_bottle_nano_V2"
+        DAN.profile['df_list'] = ['barcode_control_nano', 'syringe_control_nano', 'barcode_result_nano', 'syringe_scale_result_nano', ]
+        DAN.profile['d_name'] = self.__cfg["d_name"]
+        DAN.device_registration_with_retry(ServerURL, Reg_addr)
+        # DAN.deregister()  #if you want to deregister this device, uncomment this line
+        # exit()            #if you want to deregister this device, uncomment this line
+        while True:
+            try:
+                barcode_control = DAN.pull('barcode_control_nano')
+                if barcode_control != None:  ## barcode_control_nano -> [UserName, RandId, True]
+                    if barcode_control[-1]:
+                        self.barcode_control_info = barcode_control
+                        self.get_barcode()
+                    else:
+                        self.wait_page()
+
+                syringe_scale_control = DAN.pull('syringe_control_nano')  # Pull data from an output device feature "Dummy_Control"
+                if syringe_scale_control != None:  ## syringe_scale_control_nano -> [UserName, RandId, SyringeType, True]
+                    if syringe_scale_control[-1]:
+                        self.light.light_on(255)
+                        self.syringe_scale_control_info = syringe_scale_control
+                        self.scan_scale()
+                    else:
+                        self.wait_page()
+
+            except Exception as e:
+                self.check_network(ServerURL)
+                print(e)
+                if str(e).find('mac_addr not found:') != -1:
+                    print('Reg_addr is not found. Try to re-register...')
+                    DAN.device_registration_with_retry(ServerURL, Reg_addr)
+                else:
+                    print('Connection failed due to unknow reasons.')
+                    time.sleep(1)
+            time.sleep(0.2)
 
     ## defult Page
     def wait_page(self):
@@ -126,7 +193,6 @@ class medical_GUI:
                  font=('', int(70 * self.__font_ratio), 'bold')).place(relx=0.6, rely=0.8, relwidth=0.4, relheight=0.2)
         tk.Label(self.window, text=self.text_translate("機器編號：{}".format(self.__cfg["Mechine_ID"])), anchor="sw",
                  font=('', int(40 * self.__font_ratio), 'bold')).place(relx=0.0, rely=0.8, relwidth=0.6, relheight=0.2)
-
 
     ## Barcode Mode Page
     def get_barcode(self):  ## 輸入欄位
@@ -228,7 +294,6 @@ class medical_GUI:
                     tk.Label(self.window, text=self.text_translate("上傳中..."), anchor="e",
                              font=('', int(60 * self.__font_ratio), 'bold')).place(relx=0.0, rely=0.8, relwidth=1.0, relheight=0.2)
                     self.window.after(3000, lambda: self.wait_page())
-                    # self.wait_page()
                 else:
                     if max(self.__scale_val_hist_list) - __median > min(self.__scale_val_hist_list) - __median:
                         pop_num = max(self.__scale_val_hist_list)
@@ -236,62 +301,15 @@ class medical_GUI:
                         pop_num = min(self.__scale_val_hist_list)
                     self.__scale_val_hist_list.pop(self.__scale_val_hist_list.index(pop_num))
 
-    def check_network(self, URL="https://www.google.com.tw/", show_GUI=True):
+
+    ## Pill Mode Page
+    def pill_detector(self):  ## 輸入欄位
+        self.light.light_on(10)
         self.clean()
-        # retry_time_count = time.time()
-        try:
-            urllib.request.urlopen(URL, timeout=1)
-            if not show_GUI:
-                return True
-        except urllib.request.URLError as err:
-            if show_GUI:
-                flg = tk.Label(self.window, text="")
-                tk.Label(self.window, text=self.text_translate("系統已離線..."), font=('', int(80 * self.__font_ratio), 'bold')).place(relx=0.0, rely=0.0, relwidth=1.0, relheight=1.0)
-                tk.Button(self.window, text=self.text_translate("retry"), font=('', int(40 * self.__font_ratio), 'bold'),
-                          command=flg.pack).place(relx=0.8, rely=0.8, relwidth=0.2, relheight=0.2)
-                self.window.wait_visibility(flg)
-                self.check_network(URL)
-            else:
-                return False
-
-    def dummy_device_loop(self):
-        ServerURL = self.__cfg["ServerURL"]
-        Reg_addr = self.__cfg["Reg_addr"]  # if None, Reg_addr = MAC address
-        DAN.profile['dm_name'] = "medical_bottle_nano_V2"
-        DAN.profile['df_list'] = ['barcode_control_nano', 'syringe_control_nano', 'barcode_result_nano', 'syringe_scale_result_nano', ]
-        DAN.profile['d_name'] = self.__cfg["d_name"]
-        DAN.device_registration_with_retry(ServerURL, Reg_addr)
-        # DAN.deregister()  #if you want to deregister this device, uncomment this line
-        # exit()            #if you want to deregister this device, uncomment this line
-        while True:
-            try:
-                barcode_control = DAN.pull('barcode_control_nano')
-                if barcode_control != None:  ## barcode_control_nano -> [UserName, RandId, True]
-                    if barcode_control[-1]:
-                        self.barcode_control_info = barcode_control
-                        self.get_barcode()
-                    else:
-                        self.wait_page()
-
-                syringe_scale_control = DAN.pull('syringe_control_nano')  # Pull data from an output device feature "Dummy_Control"
-                if syringe_scale_control != None:  ## syringe_scale_control_nano -> [UserName, RandId, SyringeType, True]
-                    if syringe_scale_control[-1]:
-                        self.light.light_on(255)
-                        self.syringe_scale_control_info = syringe_scale_control
-                        self.scan_scale()
-                    else:
-                        self.wait_page()
-
-            except Exception as e:
-                self.check_network(ServerURL)
-                print(e)
-                if str(e).find('mac_addr not found:') != -1:
-                    print('Reg_addr is not found. Try to re-register...')
-                    DAN.device_registration_with_retry(ServerURL, Reg_addr)
-                else:
-                    print('Connection failed due to unknow reasons.')
-                    time.sleep(1)
-            time.sleep(0.2)
-
+        # tk.Label(self.window, text=self.text_translate("藥丸辨識"), font=('', int(40*self.__font_ratio), 'bold')).place(relx=0.0, rely=0.0, relwidth=1.0, relheight=0.1)
+        self.__pill_hint_imagetk = ImageTk.PhotoImage(Image.open('./GUI/images/HW_V2_with_pill_box.png').resize((int(self.__screen_width), int(self.__screen_height))))
+        tk.Label(self.window, image=self.__pill_hint_imagetk).place(relx=0.0, rely=0.0, relwidth=1.0, relheight=1.0)
+        ### do something
+        # self.window.after(3000, lambda: self.wait_page())
 
 
