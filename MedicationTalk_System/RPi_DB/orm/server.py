e80a534d13d403ec2d6f8ec48c2f04e0af71a043
ann88412a <ann88412a@gmail.com>
2024-01-04 19:08:17 +0800

    domain

--- MedicationTalk_System/RPi_DB/orm/server.py
+++ MedicationTalk_System/RPi_DB/orm/server.py
@@ -388,4 +388,4 @@ def index():
  
 if __name__ == "__main__":
     
-    app.run(host='0.0.0.0', port= 1526, debug=True)# , ssl_context=('server.crt', 'server.key')
+    app.run(host='0.0.0.0', port= 15260, debug=True)# , ssl_context=('server.crt', 'server.key')
